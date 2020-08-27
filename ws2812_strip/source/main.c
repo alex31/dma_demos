84077b22a5168832072b36b212f2309900a11e07
Alexandre Bustico <alexandre.bustico@enac.fr>
2020-08-27 11:11:35 +0200

    tested ok with 1 to 4 channels

--- ws2812_strip/source/main.c
+++ ws2812_strip/source/main.c
@@ -10,14 +10,13 @@
   designed to run on a STM32F407G-DISC1 board
  */
 
-/* ALL timers channels that are DMA capable:
-   T1:4, T2:4, T3:4, T4:5, T5:4, T8:4
-   So 24 possible channels with 512 leds each : 12288 leds !
-
+/* Timers with 4 channels that are DMA capable:
+   T1, T2, T3, T4:5, T5, T8
+   24 possible channels with 512 leds each : 12288 leds !
  */
 
 
-#define NB_CHANNELS 1U // between 1 and 4
+#define NB_CHANNELS 4U // between 1 and 4
 
 // cf tables 42 and 43, page 309 of stm32f4 reference manuel : RM0090
 #define STM32_TIM1_UP_DMA_STREAM                STM32_DMA_STREAM_ID(2, 5)
@@ -26,15 +25,18 @@
 #define STRIP_NB_LEDS 8U // depend of your strip, mine has 8 leds, up to 512
 
 typedef struct  {
-  uint16_t g[8]; // color is to be sent in this order : 
-  uint16_t r[8]; // g,r,b
-  uint16_t b[8];
+  uint16_t g[8][NB_CHANNELS]; // color is to be sent in this order : 
+  uint16_t r[8][NB_CHANNELS]; // g,r,b
+  uint16_t b[8][NB_CHANNELS];
 } Ws2812LedFrame;
 
 typedef struct {
-  uint16_t start[2]; // work around DMA latency on two first transfert
-  Ws2812LedFrame leds[STRIP_NB_LEDS * NB_CHANNELS]; // each bit is represented by duration coded with two bytes
-  uint16_t end[2]; // always 0 to terminate a frame
+  // always 0 to work around DMA latency silicon bug on first world
+  uint16_t start[NB_CHANNELS];
+  // each bit is represented  by duration coded with two bytes
+  Ws2812LedFrame leds[STRIP_NB_LEDS];
+  // always 0 to terminate a frame
+  uint16_t end[NB_CHANNELS]; 
 } Ws2812StripFrame;
 
 typedef enum  {SEND_COLORS, SEND_INTERFRAME} SerializeState;
@@ -62,11 +64,13 @@ static const Color purple = {100, 0, 100};
 static const Color dimmed = {10, 10, 10};    
 
 // be sure that compiler do not insert padding
-_Static_assert(sizeof(Ws2812StripFrame) == ((24 * STRIP_NB_LEDS * NB_CHANNELS) + 4) * 2);
+_Static_assert(sizeof(Ws2812StripFrame) ==
+	       ((24 * STRIP_NB_LEDS * NB_CHANNELS) + NB_CHANNELS * 2) * 2);
 
 // fonction that generate dma frame from rgb color for one led
 static void setColors(Ws2812StripFrame *ledStrip,
 		      const uint8_t ledIdx,
+		      const uint8_t chanIdx,
 		      const Color rgb);
 
 // dma configuration
@@ -91,6 +95,23 @@ static const DMAConfig dmaConfig = {
 #define WS_INTERFRAME_GAP_MILLISECOND 1U
 
 // pwm configuration
+/* DBL: this 5-bit vector defines the number of DMA transfers 
+   (the timer detects a burst transfer when a read or a write access 
+   to the TIMx_DMAR register address is performed).
+   We want to update CCR1 to CCRx depending on the number of 
+   timer channel we use
+*/
+#define    DCR_DBL       ((NB_CHANNELS-1) << 8)
+
+/* DBA: This 5-bits vector defines the base-address for DMA transfers 
+   (when read/write access are done through the TIMx_DMAR address). 
+   DBA is defined as an offset starting from the
+   address of the TIMx_CR1 register.
+   first register to set is CCR1
+*/
+#define DCR_DBA(pwmd)    (((uint32_t *) (&pwmd.tim->CCR) - \
+			  ((uint32_t *) (&pwmd.tim->CR1))))
+
 static const PWMConfig  pwmCfg = {
 	    .frequency = 84U * 1000U * 1000U, 
 	    .period    = WS_PERIOD, 
@@ -145,7 +166,11 @@ int main(void) {
 			  .stateCnt = 0};
   
   const Color	colorArraySource[NB_CHANNELS][STRIP_NB_LEDS] =
-    {{red, green, blue, yellow, white, cyan, purple, dimmed}};
+    {{red, green, blue, yellow, white, cyan, purple, dimmed},
+     {green, blue, yellow, green, blue, yellow, green, blue},
+     {cyan, purple, cyan, purple, cyan, purple, cyan, purple},
+     {white, dimmed, white, dimmed, white, dimmed, white, dimmed}
+    };
 
   halInit();
   chSysInit();
@@ -168,10 +193,14 @@ int main(void) {
   // start timer in pwm mode
   pwmStart(&PWMD1, &pwmCfg);
   
-  // activate channel 0 where led strip is plugged
+  // enable bloc register DMA transaction through DMAR register
+  PWMD1.tim->DCR = DCR_DBL | DCR_DBA(PWMD1); 
+
+  // activate enabled channels where led strips are plugged
   // until first frame is sent, only 0 bit are sent
-  // to lightoff the strip
-  pwmEnableChannel(&PWMD1, 0, WS_T0H);
+  // to lightoff the strips
+  for (size_t i=0; i< NB_CHANNELS; i++)
+    pwmEnableChannel(&PWMD1, i, WS_T0H);
 
   // start the interractive shell
   consoleLaunch();  
@@ -190,12 +219,12 @@ int main(void) {
 	colorArray[i][j].r *= sina;
 	colorArray[i][j].g *= sina;
 	colorArray[i][j].b *= sina;
-	setColors(&ledStrip.frame, j+(i*NB_CHANNELS), colorArray[i][j]);
+	setColors(&ledStrip.frame, j, i, colorArray[i][j]);
       }
     }
 
     // send the frame to the led strip
-    dmaTransfert(&dmap, &PWMD1.tim->CCR, ledStrip.frame.start,
+    dmaTransfert(&dmap, &PWMD1.tim->DMAR, ledStrip.frame.start,
 		 sizeof(ledStrip.frame) / sizeof(uint16_t));
     chThdSleepMilliseconds(10); // 100 hz
     angle = fmod(angle += 0.05f, 3.141592f);
@@ -203,25 +232,26 @@ int main(void) {
  }
 
 
-
+typedef uint16_t (*ColorBitsArray)[NB_CHANNELS];
 static void setColor(const uint8_t col,
-		     uint16_t * const bitArray)
+		     const uint8_t chanIdx,
+		     ColorBitsArray const bitArray)
 {
   // each bit is serialised with pulde width
   // MSB is serialised first
   //  DebugTrace("bitArray@%p = %u", bitArray, col);
   for (size_t i=0; i<8; i++)
-    bitArray[i] = col & (1U<<(7U-i)) ?  WS_T1H : WS_T0H;
-  
+    bitArray[i][chanIdx] = col & (1U<<(7U-i)) ?  WS_T1H : WS_T0H;
 }
 
 static void setColors(Ws2812StripFrame *ledStrip,
-		     const uint8_t ledIdx,
-		     const Color rgb)
+		      const uint8_t ledIdx,
+		      const uint8_t chanIdx,
+		      const Color rgb)
 {
   Ws2812LedFrame * const led = &ledStrip->leds[ledIdx];
-  setColor(rgb.g, led->g);
-  setColor(rgb.r, led->r);
-  setColor(rgb.b, led->b);
+  setColor(rgb.g, chanIdx, led->g);
+  setColor(rgb.r, chanIdx, led->r);
+  setColor(rgb.b, chanIdx, led->b);
 }
 
