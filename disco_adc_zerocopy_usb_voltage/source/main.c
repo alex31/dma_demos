739335741b78ae2e71ef084511b486caddc8050c
Alexandre Bustico <alexandre.bustico@enac.fr>
2021-02-05 14:01:40 +0100

    remove adcHelper usage and directy fill ADC1 register

--- disco_adc_zerocopy_usb_voltage/source/main.c
+++ disco_adc_zerocopy_usb_voltage/source/main.c
@@ -2,7 +2,6 @@
 #include <hal.h>
 #include "stdutil.h"
 #include "ttyConsole.h"
-#include "adcHelper.h"
 #include "hal_stm32_dma.h"
 
 /*
@@ -47,8 +46,6 @@ static void blinker (void *arg)
   }
 }
 
-ADCConversionGroup adcgrpcfg;
-
 // theses objects will manage a stream of a dma controller
 DMADriver dmapAdc1;
 
@@ -71,7 +68,7 @@ objects_fifo_t fifo;
 
 
 static void findVoltageSpike(const fifo_dmaBuf_t *samples);
-static void initAdc1(const ADCConversionGroup *grpp);
+static void initAdc1(void);
 
 int main(void)
 {
@@ -96,12 +93,8 @@ int main(void)
   chThdCreateStatic(waBlinker, sizeof(waBlinker), NORMALPRIO, &blinker, NULL);
 
   // helper function that fill ADCConversionGroup : one shot mode
-  adcgrpcfg = adcGetConfig(ADC_GRP1_NUM_CHANNELS,
-			   ADC_CHANNEL_IN11, ADC_CYCLES_480,
-			   ADC_END);
 
-
-  initAdc1(&adcgrpcfg);
+  initAdc1();
   dmaObjectInit(&dmapAdc1);
   dmaStart(&dmapAdc1, &dmaConfigAdc1);
   dmaStartTransfert(&dmapAdc1, &ADC1->DR, NULL, DMA_TRANSACTION_SIZE);
@@ -176,9 +169,10 @@ static void findVoltageSpike(const fifo_dmaBuf_t *samples)
 
 
 
-static void initAdc1(const ADCConversionGroup *grpp)
+static void initAdc1()
 {
-  uint32_t cr2;
+  const uint32_t cr2 = ADC_CR2_SWSTART | ADC_CR2_DMA | ADC_CR2_DDS | ADC_CR2_ADON;;
+
   rccEnableADC1(true);
   ADC->CCR = (ADC->CCR & (ADC_CCR_TSVREFE | ADC_CCR_VBATE)) |
     (STM32_ADC_ADCPRE << 16);
@@ -188,30 +182,24 @@ static void initAdc1(const ADCConversionGroup *grpp)
   ADC1->CR2 = 0;
   ADC1->CR2 = ADC_CR2_ADON;
   ADC1->SR    = 0;
-  ADC1->SMPR1 = grpp->smpr1;
-  ADC1->SMPR2 = grpp->smpr2;
-  ADC1->HTR   = grpp->htr;
-  ADC1->LTR   = grpp->ltr;
-  ADC1->SQR1  = grpp->sqr1 | ADC_SQR1_NUM_CH(grpp->num_channels);
-  ADC1->SQR2  = grpp->sqr2;
-  ADC1->SQR3  = grpp->sqr3;
-  /* ADC configuration and start.*/
-  ADC1->CR1   = grpp->cr1 | ADC_CR1_OVRIE | ADC_CR1_SCAN;
+  ADC1->SMPR1 = ADC_SMPR1_SMP_AN11(ADC_SAMPLE_480);
+  ADC1->SMPR2 = 0;
+  ADC1->HTR   = 0;
+  ADC1->LTR   = 0;
+  ADC1->SQR1  = ADC_SQR1_NUM_CH(1);
+  ADC1->SQR2  = 0;
+  ADC1->SQR3  = ADC_SQR3_SQ1_N(ADC_CHANNEL_IN11);
 
-  /* Enforcing the mandatory bits in CR2.*/
-  cr2 = grpp->cr2 | ADC_CR2_DMA | ADC_CR2_DDS | ADC_CR2_ADON;
+  /* ADC configuration and start.*/
+  ADC1->CR1   = ADC_CR1_OVRIE | ADC_CR1_SCAN;
 
   /* The start method is different dependign if HW or SW triggered, the
      start is performed using the method specified in the CR2 configuration.*/
-  if ((cr2 & ADC_CR2_SWSTART) != 0) {
-    /* Initializing CR2 while keeping ADC_CR2_SWSTART at zero.*/
-    ADC1->CR2 = (cr2 | ADC_CR2_CONT) & ~ADC_CR2_SWSTART;
-
-    /* Finally enabling ADC_CR2_SWSTART.*/
-    ADC1->CR2 = (cr2 | ADC_CR2_CONT);
-  }
-  else
-    ADC1->CR2 = cr2;
+  /* Initializing CR2 while keeping ADC_CR2_SWSTART at zero.*/
+  ADC1->CR2 = (cr2 | ADC_CR2_CONT) & ~ADC_CR2_SWSTART;
+  
+  /* Finally enabling ADC_CR2_SWSTART.*/
+  ADC1->CR2 = (cr2 | ADC_CR2_CONT);
 }
 
 
